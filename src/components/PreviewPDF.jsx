a0ec33539ff4c6fa02d6f13e56216537eb3ba187
Jean Coppieters <jean.coppieters@hotmail.com>
2025-12-28 18:33:16 +0000

    Minor

--- src/components/PreviewPDF.jsx
+++ src/components/PreviewPDF.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import CartaAima from "./CartaAima";
 
 const PreviewPDF = ({formData, date}) => {
-
     return (
         <div className="preview-section">
             <h3>Pré-visualização</h3>
